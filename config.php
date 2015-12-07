596d01951c51298139bdc267f5e2d5e9a73ade0c
srgg6701 <srgg67@gmail.com>
2015-12-07 23:14:25 +0300

    Добавлен config.

---
+++ config.php
@@ -0,0 +1,2 @@
+<?php
+define('APP_PATH',$_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'php-client-sdk');
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once 'config.php';?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +6,18 @@
   <title>Home</title>
 </head>
 <body>
-<h1>Hello, researcher!</h1>
+<h1>Hello, researcher!</h1><?php
+
+echo "<div>APP_PATH: ".APP_PATH."</div>";
+
+/*require_once("ApiClient.php");
+$API_KEY = "your_api_key";
+$BASE_PATH = "http://api.cortical.io/rest";
+$RETINA_NAME = "en_associative";
+$apiClient = new APIClient($API_KEY, $BASE_PATH);
+$termsApi = new TermsApi($apiClient);
+
+$terms = $termsApi->getTerm("apple", true, $RETINA_NAME);
+echo serialize($termArr[0]->fingerprint->positions);*/?>
 </body>
 </html>
