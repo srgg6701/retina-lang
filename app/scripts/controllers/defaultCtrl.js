b1d1211c34225293f5c8039b71386c9f79659a34
srgg6701 <srgg67@gmail.com>
2016-02-23 21:12:51 +0300

    The common layout is added.

--- app/scripts/controllers/defaultCtrl.js
+++ app/scripts/controllers/defaultCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller('defaultCtrl', function($scope, $state, $rootScope){
+app.controller('defaultCtrl', function($scope, $state, $rootScope, testFactory){
     //console.log('defaultCtrl works, url:', $state.current);
     $rootScope.$on('$stateChangeStart',
         function(event, toState, toParams, fromState, fromParams, options){
@@ -9,5 +9,37 @@ app.controller('defaultCtrl', function($scope, $state, $rootScope){
             console.log('event, toState, toParams, fromState, fromParams, options',
             {   event:event, toState:toState, toParams:toParams,
                 fromState:fromState, fromParams:fromParams, options:options });
+
+            $scope.section = toState.name;
+            if(toState.name=='home'){
+            }else{
+                $scope.section = toState.name.split('.').pop();
+                console.log('$scope.section', $scope.section);
+                switch ($scope.section) {
+                    case 'retina':
+
+                        break;
+                    case 'term':
+
+                        break;
+                    case 'text':
+
+                        break;
+                    case 'expression':
+
+                        break;
+                    case 'compare':
+
+                        break;
+                    case 'image':
+
+                        break;
+                    case 'classify':
+
+                        break;
+                }
+            }
+
         });
+    console.log('testFactory', testFactory);
 });
