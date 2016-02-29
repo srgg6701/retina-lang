d82188c346f85df2543b81a906d5ce8ea5cd44e9
srgg6701 <srgg67@gmail.com>
2016-03-01 01:33:46 +0300

    Settings some API methods calling, creating new services for handle response, refactoring.

--- app/scripts/controllers/defaultCtrl.js
+++ app/scripts/controllers/defaultCtrl.js
@@ -13,60 +13,45 @@ app.controller('defaultCtrl', function($scope, $state, $rootScope, manageData){
             $scope.section = toState.name.split('.').pop(); //console.log('section', $scope.section);
             // make page title
             $scope.title = $scope.section.charAt(0).toUpperCase() + $scope.section.slice(1);
-            if(toState.name=='home'){
-            }else{
-                switch ($scope.section) {
-                    case 'retina':
-
-                        break;
-                    case 'term':
-
-                        break;
-                    case 'text':
-
-                        break;
-                    case 'expression':
-
-                        break;
-                    case 'compare':
-
-                        break;
-                    case 'image':
-
-                        break;
-                    case 'classify':
-
-                        break;
-                }
-            }
         });
 });
-app.controller('contentsCtrl', function($scope, $rootScope, selects){
+app.controller('contentsCtrl', function($scope, $rootScope, selects, manageFormParams, getResponse){
     // set subsection and retina_name defaults
     $scope.form={};
     $scope.helpvisible={};
     // watch if the subsection has been changed
-    $scope.$watch('form.param', function(newValue, oldValue){
+    $scope.$watch('form.subsection', function(newValue, oldValue){
         console.log(oldValue+'=>'+newValue);
+        if(newValue=='terms'){
+            $scope.form.termReq=false;
+        }else if(newValue=='terms/contexts'||newValue=='terms/similar_terms'){
+            if(!$scope.form.term) $scope.form.termReq=true;
+        }
+        console.log('1. form.termReq', $scope.form.termReq);
+    });
+    // if term field value has been changed
+    $scope.$watch('form.term', function(newValue, oldValue){
+        if($scope.form.subsection=='terms/contexts'||$scope.form.subsection=='terms/similar_terms')
+            if(newValue) $scope.form.termReq=false;
+        console.log('2. form.termReq', $scope.form.termReq);
     });
 
     $rootScope.$on('$stateChangeStart',
         function(event, toState, toParams, fromState, fromParams, options){
             $scope.section = toState.name.split('.').pop();
             $scope.selectData = selects[$scope.section];
-            // handle required fields
-            $scope.form.body=null;
-            $scope.form.retinaName=null;
-            $scope.form.param=null;
-            $scope.form.filterName=true;
-            if($scope.section=='retina'||$scope.section=='classify'){
-                $scope.form.param=true;
-            }
-            if($scope.section=='retina'||$scope.section=='term'){
-                $scope.form.body=true;
-            }
-            if($scope.section=='classify'){
-                $scope.form.filterName=null;
-            }
+            manageFormParams.manageFormData($scope);
     });
+
+
+    var apiKey = '414898c0-9c58-11e5-9e69-03c0722e0d16',
+        //liteClient = retinaSDK.LiteClient(apiKey),
+        fullClient = retinaSDK.FullClient(apiKey);
+    $scope.getRetinaData = function(event){
+        var formElement = angular.element(event.currentTarget),
+            form = $scope.form;
+        console.log('title, form, fullClient, scope', { section:$scope.section, form:$scope.form, fullClient:fullClient, 'scope':$scope });
+        getResponse.getResponseData($scope, $scope.form);
+    }
+
 });
--- app/scripts/services/manageDataService.js
+++ app/scripts/services/manageDataService.js
@@ -8,3 +8,69 @@ app.service('manageData', function($http, $q){
         return deferred.promise;
     };
 });
+app.service('manageFormParams', function(){
+    this.manageFormData = function(scope){
+        scope.form.body=null;
+        scope.form.retinaName=null;
+        scope.form.subsection=null;
+        scope.form.filterName=true;
+        scope.form.termReq=false;
+        if(scope.section=='retina'||scope.section=='classify'){
+            scope.form.subsection=true;
+        }
+        if(scope.section=='retina'||scope.section=='term'){
+            scope.form.body=true;
+        }
+        if(scope.section=='classify'){
+            scope.form.filterName=null;
+        }    
+    };
+});
+app.service('getResponse', function(){
+    this.getResponseData = function(scope, form){
+        var data;
+        switch (scope.section) {
+            case 'retina':
+                // optional: retina_name (string)
+                data = fullClient.getRetinas(form.retinaName); // console.log('retina_name', form.retinaName);
+                break;
+            case 'term':
+                switch (form.subsection) {
+                    case 'terms':
+                        /*  optional: term (string), start_index (number), max_results (number), get_fingerprint (boolean) */
+                        data = fullClient.getTerms(term, start_index, max_results, get_fingerprint);
+                        break;
+                    case 'terms/contexts':
+                        /*  required: term
+                         optional: start_index (number), max_results (number), get_fingerprint (boolean)   */
+                        data = fullClient.getContextsForTerm(term, start_index, max_results, get_fingerprint);
+                        break;
+                    case 'terms/similar_terms':
+                        /*  required: term
+                         optional: context_id (number), start_index (number), max_results (number), pos_type (string), get_fingerprint (boolean)  */
+                        data = fullClient.getSimilarTermsForTerm(term, context_id, start_index, max_results, pos_type, get_fingerprint);
+                        break;
+                }
+
+                break;
+            case 'text':
+                /*  required: term (string)
+                 optional: start_index (number), max_results (number), get_fingerprint (boolean)*/
+                //data = fullClient.(term, start_index, max_results, get_fingerprint);
+                break;
+            case 'expression':
+                /**/
+                //data = fullClient.getFingerprintForText();
+                break;
+            case 'compare':
+                //data = fullClient.();
+                break;
+            case 'image':
+                //data = fullClient.();
+                break;
+            case 'classify':
+                //data = fullClient.();
+                break;
+        }
+    };
+});
